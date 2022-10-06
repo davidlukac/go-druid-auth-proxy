3133fe6150aab33fd6ece799d07b6cca409e9c60
David Lukac <1215290+davidlukac@users.noreply.github.com>
2022-10-05 17:59:02 -0700

    Initial pipeline

--- bin/docker-build.sh
+++ bin/docker-build.sh
@@ -7,11 +7,17 @@ source .env
 declare APP_VER
 declare APP_IMG
 
+mkdir -p build
+
+# Build.
 docker build . \
   --build-arg APP_VER="${APP_VER}" \
   --target export \
   --output type=local,dest=build
 
+ls -al build
+
+# Package
 docker build . \
   --file package.Dockerfile \
   --build-arg APP_VER="${APP_VER}" \
