4e38651d17e2cc06c252a5bd6a66e681cabcb948
David Lukac <1215290+davidlukac@users.noreply.github.com>
2022-10-05 18:16:17 -0700

    Initial pipeline

--- bin/docker-build.sh
+++ bin/docker-build.sh
@@ -1,5 +1,8 @@
 #!/usr/bin/env bash
 
+# Arguments
+# 1 - if provided used as image suffix
+
 set -xe
 
 source .env
@@ -21,4 +24,4 @@ ls -al build
 docker build . \
   --file package.Dockerfile \
   --build-arg APP_VER="${APP_VER}" \
-  -t "${APP_IMG}":"${APP_VER}"-pipeline-test
+  -t "${APP_IMG}":"${APP_VER}${1}"
